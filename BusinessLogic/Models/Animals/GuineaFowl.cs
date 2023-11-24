196fd6d925a83e03c877669f68e9a7513fb6b562
MoeyCC <mohan.ratnam@cornwall.gov.uk>
2023-11-24 16:07:55 +0000

    Added GuineaFowl

---
+++ BusinessLogic/Models/Animals/GuineaFowl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zoo.BusinessLogic.Models.Animals
+{
+  public class GuineaFowl : Animal, ICanBeGroomed, ISmallAnimal
+  {
+    private DateTime lastGroomed;
+
+    public GuineaFowl(DateTime dateOfBirth) : base(dateOfBirth)
+    {
+    }
+
+    public override void Feed()
+    {
+      Console.WriteLine("<Cluck, Cluck>");
+      base.Feed();
+    }
+
+    public void Groom()
+    {
+      lastGroomed = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+      return base.ToString() + $"; Last Groomed {lastGroomed}";
+    }
+  }
+}
--- ZooManagement-CSharp/Program.cs
+++ ZooManagement-CSharp/Program.cs
@@ -22,6 +22,7 @@ namespace Zoo.ConsoleApp
             };
             var smallAnimals = new ISmallAnimal[] {
                 new Rabbit(new DateTime(2014, 1, 1)),
+                new GuineaFowl(new DateTime(2010, 3, 14))
             };
             var animals = largeAnimals.Union<IAnimal>(smallAnimals).ToList();
 
